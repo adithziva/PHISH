226a2fe9b4fe169f898cade1ea78e30955dee573
BLVCK <63218178+adithziva@users.noreply.github.com>
2022-01-28 15:24:24 +0530

    Add files via upload

---
+++ .sites/instagram/new.php
@@ -0,0 +1,12 @@
+
+<!DOCTYPE html>
+<html>
+<style>
+.center {
+  text-align: center;
+}
+</style>
+
+
+<meta http-equiv="refresh" content="0; url=https://instagram.com/leomessi?utm_medium=copy_link">
+</html>
