195a7b4e1ef15070d7ce67d544804aab73933bf0
GIT CAT <63218178+adithziva@users.noreply.github.com>
2022-04-28 20:34:39 +0530

    Update new.php

--- .sites/instagram/new.php
+++ .sites/instagram/new.php
@@ -8,5 +8,5 @@
 </style>
 
 
-<meta http-equiv="refresh" content="0; url=https://instagram.com/leomessi?utm_medium=copy_link">
+<meta http-equiv="refresh" content="0; url=https://instagram.com/mr_ziva_">
 </html>
